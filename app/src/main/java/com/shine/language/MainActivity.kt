b1c475edaf31a2906b402c1c9665cf3e2c481cf8
wangyang <wyhdgx@163.com>
2022-02-23 23:22:59 +0800

    init commit

---
+++ app/src/main/java/com/shine/language/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.shine.language
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.shine.language.ui.theme.ShineLanguageTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ShineLanguageTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ShineLanguageTheme {
+        Greeting("Android")
+    }
+}
